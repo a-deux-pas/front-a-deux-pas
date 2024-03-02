54db5457ecf277bbcb29ede4f6043c9295490218
LEA HADIDA <hadida.lea@gmail.com>
2024-03-02 09:46:25 +0100

    re-organization of the files

---
+++ src/app/shared/components/tabs-account/tabs-account.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TabsAccountComponent } from './tabs-account.component';
+
+describe('TabsAccountComponent', () => {
+  let component: TabsAccountComponent;
+  let fixture: ComponentFixture<TabsAccountComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [TabsAccountComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(TabsAccountComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/shared/components/tabs-account/tabs-account.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tabs-account',
+  templateUrl: './tabs-account.component.html',
+  styleUrl: './tabs-account.component.scss'
+})
+export class TabsAccountComponent {
+
+}
