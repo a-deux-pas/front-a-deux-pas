cb4ba4f0059bc9d46ae2e6095c2546a2a8de6db1
Mircea Bardan <mircea.c.bardan@gmail.com>
2024-03-22 12:29:21 +0100

    commented code

--- src/app/shared/components/ads/ad-list/ad-list.service.ts
+++ src/app/shared/components/ads/ad-list/ad-list.service.ts
@@ -10,29 +10,7 @@ export class AdListService {
   constructor(private http: HttpClient) {}
 
   // fetch all ads
-  fetchAdsList(): Observable<HomePageAd[]> {
+  fetchAdList(): Observable<HomePageAd[]> {
     return this.http.get<HomePageAd[]>('http://localhost:8081/annonces/liste');
-  }
-
-  // fetch the ads that match the filtering criteria passed as query params
-  fetchFilteredAds(
-    selectedPriceRanges: string[],
-    selectedCitiesAndPostalCodes: string[],
-    selectedArticleStates: string[],
-    selectedCategories: string[],
-    selectedSubcategories: string[],
-    selectedGender: string[]
-  ): Observable<HomePageAd[]> {
-    const queryParams = {
-      priceRanges: selectedPriceRanges.join(','),
-      citiesAndPostalCodes: selectedCitiesAndPostalCodes.join(','),
-      articleStates: selectedArticleStates.join(','),
-      categories: selectedCategories.join(','),
-      subcategories: selectedSubcategories.join(','),
-      gender: selectedGender.join(','),
-    };
-    return this.http.get<HomePageAd[]>('http://localhost:8081/annonces/liste', {
-      params: queryParams,
-    });
   }
 }
