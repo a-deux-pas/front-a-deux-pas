ead5c8e5898c9b2fab2587d28b6fc7ce49dcd81e
Erika IKELEMPO <erika.ikelempo@algonis.net>
2024-04-06 22:00:43 +0200

    Not sure to keep it

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,22 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'compte/profil',
-    loadChildren: () => import('./routes/account/profile/profile.module').then(m => m.ProfileModule)
-  },
-  {
-    path: 'compte/annonces',
-    loadChildren: () => import('./routes/account/ads/ads.module').then(m => m.AdsModule)
-  },
-  {
-    path: 'compte/rdv',
-    loadChildren: () => import('./routes/account/meetings/meetings.module').then(m => m.MeetingsModule)
-  },
-  {
-    path: 'compte/favoris',
-    loadChildren: () => import('./routes/account/favorites/favorites.module').then(m => m.FavoritesModule)
-  }
+  // {
+  //   path: 'compte/profil',
+  //   loadChildren: () => import('./routes/account/profile/profile.module').then(m => m.ProfileModule)
+  // },
+  // {
+  //   path: 'compte/annonces',
+  //   loadChildren: () => import('./routes/account/ads/ads.module').then(m => m.AdsModule)
+  // },
+  // {
+  //   path: 'compte/rdv',
+  //   loadChildren: () => import('./routes/account/meetings/meetings.module').then(m => m.MeetingsModule)
+  // },
+  // {
+  //   path: 'compte/favoris',
+  //   loadChildren: () => import('./routes/account/favorites/favorites.module').then(m => m.FavoritesModule)
+  // }
 ];
 
 @NgModule({
--- src/app/shared/tests/unit/profile.cy.ts
+++ src/app/shared/tests/unit/profile.cy.ts
@@ -1,49 +1,49 @@
-import { HttpClientModule } from "@angular/common/http";
-import { ProfileComponent } from "../../../routes/account/profile/profile.component";
-import { ProfileService } from "../../../routes/account/profile/profile.service";
-import { SharedComponentsModule } from "../../components/shared-components.module";
-import { MeetingPlacesComponent } from "../../../routes/account/profile/components/meeting-places/meeting-places.component";
-import { RouterTestingModule } from "@angular/router/testing";
-import { EditButtonComponent } from "../../../routes/account/profile/components/edit-button/edit-button.component";
+// import { HttpClientModule } from "@angular/common/http";
+// import { ProfileComponent } from "../../../routes/account/profile/profile.component";
+// import { ProfileService } from "../../../routes/account/profile/profile.service";
+// import { SharedComponentsModule } from "../../components/shared-components.module";
+// import { MeetingPlacesComponent } from "../../../routes/account/profile/components/meeting-places/meeting-places.component";
+// import { RouterTestingModule } from "@angular/router/testing";
+// import { EditButtonComponent } from "../../../routes/account/profile/components/edit-button/edit-button.component";
 
-describe('Profile component', () => {
-    beforeEach(() => {
-      cy.intercept('http://localhost:8081/api/account/profile/presentation', { fixture: 'user-profile' }).as('getUserPresentation');
-      cy.intercept('http://localhost:8081/api/account/profile/schedules', { fixture: 'user-preferred-schedule' }).as('getUserPreferredSchedules');
-      cy.intercept('http://localhost:8081/api/account/profile/meeting-places', { fixture: 'user-meeting-places' }).as('getPreferredMeetingPlaces');
+// describe('Profile component', () => {
+//     beforeEach(() => {
+//       cy.intercept('http://localhost:8081/api/account/profile/presentation', { fixture: 'user-profile' }).as('getUserPresentation');
+//       cy.intercept('http://localhost:8081/api/account/profile/schedules', { fixture: 'user-preferred-schedule' }).as('getUserPreferredSchedules');
+//       cy.intercept('http://localhost:8081/api/account/profile/meeting-places', { fixture: 'user-meeting-places' }).as('getPreferredMeetingPlaces');
 
-    cy.mount(ProfileComponent, {
-      declarations: [MeetingPlacesComponent, EditButtonComponent],
-      imports: [HttpClientModule, SharedComponentsModule, RouterTestingModule],
-      providers: [ProfileService],
-      componentProperties: {
-        onEditModeChange: cy.spy().as('onEditModeChange'),
-      },
-    }).then(() => {
-      cy.wait('@getUserPresentation').its('response.statusCode').should('eq', 200);
-      cy.wait('@getUserPreferredSchedules').its('response.statusCode').should('eq', 200);
-      cy.wait('@getPreferredMeetingPlaces').its('response.statusCode').should('eq', 200);
-    });
-  });
+//     cy.mount(ProfileComponent, {
+//       declarations: [MeetingPlacesComponent, EditButtonComponent],
+//       imports: [HttpClientModule, SharedComponentsModule, RouterTestingModule],
+//       providers: [ProfileService],
+//       componentProperties: {
+//         onEditModeChange: cy.spy().as('onEditModeChange'),
+//       },
+//     }).then(() => {
+//       cy.wait('@getUserPresentation').its('response.statusCode').should('eq', 200);
+//       cy.wait('@getUserPreferredSchedules').its('response.statusCode').should('eq', 200);
+//       cy.wait('@getPreferredMeetingPlaces').its('response.statusCode').should('eq', 200);
+//     });
+//   });
 
-  it('should trigger edit mode change event for presentation section', () => {
-    cy.get('#presentation #edit-button-container .btn-icon').click();
-    cy.get('@onEditModeChange').should('have.been.calledOnceWith', true, 'presentation');
-    cy.get('#presentation #edit-button-container .save-icon').click();
-    //TODO : check if data saved
-  });
+//   it('should trigger edit mode change event for presentation section', () => {
+//     cy.get('#presentation #edit-button-container .btn-icon').click();
+//     cy.get('@onEditModeChange').should('have.been.calledOnceWith', true, 'presentation');
+//     cy.get('#presentation #edit-button-container .save-icon').click();
+//     //TODO : check if data saved
+//   });
 
-  it('should trigger edit mode change event for schedule section', () => {
-    cy.get('#schedule #edit-button-container .btn-icon').click();
-    cy.get('@onEditModeChange').should('have.been.calledOnceWith', true, 'schedule');
-    cy.get('#schedule #edit-button-container .save-icon').click();
-     //TODO : check if data saved
-  });
+//   it('should trigger edit mode change event for schedule section', () => {
+//     cy.get('#schedule #edit-button-container .btn-icon').click();
+//     cy.get('@onEditModeChange').should('have.been.calledOnceWith', true, 'schedule');
+//     cy.get('#schedule #edit-button-container .save-icon').click();
+//      //TODO : check if data saved
+//   });
 
-  it('should trigger edit mode change event for meeting places section', () => {
-    cy.get('#meeting-places #edit-button-container .btn-icon').click();
-    cy.get('@onEditModeChange').should('have.been.calledOnceWith', true, 'meeting-places');
-    cy.get('#meeting-places #edit-button-container .save-icon').click();
-    //TODO : check if data saved
-  });
-});
+//   it('should trigger edit mode change event for meeting places section', () => {
+//     cy.get('#meeting-places #edit-button-container .btn-icon').click();
+//     cy.get('@onEditModeChange').should('have.been.calledOnceWith', true, 'meeting-places');
+//     cy.get('#meeting-places #edit-button-container .save-icon').click();
+//     //TODO : check if data saved
+//   });
+// });
