64d121fadd6a9a2f915188fa732c532728a13182
Mircea Bardan <mircea.c.bardan@gmail.com>
2024-04-15 22:31:03 +0200

    added magnifying glass icon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,8 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { StyleGuideComponent } from './style-guide/style-guide.component';
-import { StyleGuideRoutingModule } from './style-guide/style-guide-routing.module';
+import { StyleGuideComponent } from '../style/style-guide/style-guide.component';
+import { StyleGuideRoutingModule } from '../style/style-guide/style-guide-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { DefaultHomeComponent } from './routes/home/default-home/default-home.component';
 import { AdListComponent } from './shared/components/ads/ad-list/ad-list.component';
