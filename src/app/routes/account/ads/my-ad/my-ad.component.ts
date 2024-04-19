a17355f59f7658f0bcef6d78aa285c41edb5d0f4
Erika IKELEMPO <erika.ikelempo@algonis.net>
2024-04-19 12:39:53 +0200

    added ngbCarousel for image mobile view

--- src/app/routes/account/ads/my-ad/my-ad.component.ts
+++ src/app/routes/account/ads/my-ad/my-ad.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AdService } from '../../../Ad.service';
 import { AdResponse } from '../../../../../model/adResponse.model';
-import { NgSwitch, NgSwitchCase } from '@angular/common';
+import { NgbCarousel, NgbSlideEvent, NgbSlideEventSource } from '@ng-bootstrap/ng-bootstrap';
+
 
 
 @Component({
@@ -40,6 +41,39 @@ export class MyAdComponent implements OnInit {
         console.error(error);
       }
     });
+  }
+
+
+  // image carrousel for mobile device
+
+  @ViewChild('carousel', { static: true }) carousel!: NgbCarousel;
+
+  paused = false;
+  unpauseOnArrow = false;
+  pauseOnIndicator = false;
+  pauseOnHover = true;
+  pauseOnFocus = true;
+
+  togglePaused() {
+    if (this.paused) {
+      this.carousel!.cycle();
+    } else {
+      this.carousel!.pause();
+    }
+    this.paused = !this.paused;
+  }
+
+  onSlide(slideEvent: NgbSlideEvent) {
+    if (
+      this.unpauseOnArrow &&
+      slideEvent.paused &&
+      (slideEvent.source === NgbSlideEventSource.ARROW_LEFT || slideEvent.source === NgbSlideEventSource.ARROW_RIGHT)
+    ) {
+      this.togglePaused();
+    }
+    if (this.pauseOnIndicator && !slideEvent.paused && slideEvent.source === NgbSlideEventSource.INDICATOR) {
+      this.togglePaused();
+    }
   }
 }
 
