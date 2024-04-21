745cecd09619451a7465c7e7fa0ac9b15c8d89c6
Erika IKELEMPO <erika.ikelempo@algonis.net>
2024-04-21 07:48:32 +0200

    put the upload picture variable back in service

--- src/services/upload-picture.service.ts
+++ src/services/upload-picture.service.ts
@@ -1,28 +1,27 @@
-// import { Injectable } from '@angular/core';
-// import { HttpClient } from '@angular/common/http';
-// import { Observable, forkJoin } from 'rxjs';
-// import { UPLOAD_PRESET, CLOUD_NAME, CALL_TO_CLOUDINARY_API } from '../app/shared/utils/credientials';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, forkJoin } from 'rxjs';
 
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class UploadPictureService {
+@Injectable({
+  providedIn: 'root'
+})
+export class UploadPictureService {
 
-//   constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-//   completeDataToUpload(file: File): Observable<any> {
-//     const data = new FormData();
-//     data.append('file', file);
-//     data.append('upload_preset', `${UPLOAD_PRESET}`);
-//     data.append('cloud_name', `${CLOUD_NAME}`);
-//     return this.http.post(`${CALL_TO_CLOUDINARY_API}`, data);
-//   }
+  completeDataToUpload(file: File): Observable<any> {
+    const data = new FormData();
+    data.append('file', file);
+    data.append('upload_preset', `adeupasProject`);
+    data.append('cloud_name', `erikaadeuxpas`);
+    return this.http.post(`https://api.cloudinary.com/v1_1/erikaadeuxpas/upload/`, data);
+  }
 
-//   uploadImages(files: File[]): Observable<any[]> {
-//     const observables: Observable<any>[] = [];
-//     files.forEach(file => {
-//       observables.push(this.completeDataToUpload(file));
-//     });
-//     return forkJoin(observables);
-//   }
-// }
+  uploadImages(files: File[]): Observable<any[]> {
+    const observables: Observable<any>[] = [];
+    files.forEach(file => {
+      observables.push(this.completeDataToUpload(file));
+    });
+    return forkJoin(observables);
+  }
+}
