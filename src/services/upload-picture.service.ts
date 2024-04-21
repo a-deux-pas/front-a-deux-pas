f3b57dd32f7695e9a37127442cfa6ba4526d5218
Erika IKELEMPO <erika.ikelempo@algonis.net>
2024-04-21 07:34:30 +0200

    commented upload picture service

--- src/services/upload-picture.service.ts
+++ src/services/upload-picture.service.ts
@@ -1,30 +1,28 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable, forkJoin } from 'rxjs';
-import { UPLOAD_PRESET } from '../app/shared/utils/credientials';
-import { CLOUD_NAME } from '../app/shared/utils/credientials';
-import { CALL_TO_CLOUDINARY_API } from '../app/shared/utils/credientials';
+// import { Injectable } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+// import { Observable, forkJoin } from 'rxjs';
+// import { UPLOAD_PRESET, CLOUD_NAME, CALL_TO_CLOUDINARY_API } from '../app/shared/utils/credientials';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class UploadPictureService {
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class UploadPictureService {
 
-  constructor(private http: HttpClient) { }
+//   constructor(private http: HttpClient) { }
 
-  completeDataToUpload(file: File): Observable<any> {
-    const data = new FormData();
-    data.append('file', file);
-    data.append('upload_preset', `${UPLOAD_PRESET}`);
-    data.append('cloud_name', `${CLOUD_NAME}`);
-    return this.http.post(`${CALL_TO_CLOUDINARY_API}`, data);
-  }
+//   completeDataToUpload(file: File): Observable<any> {
+//     const data = new FormData();
+//     data.append('file', file);
+//     data.append('upload_preset', `${UPLOAD_PRESET}`);
+//     data.append('cloud_name', `${CLOUD_NAME}`);
+//     return this.http.post(`${CALL_TO_CLOUDINARY_API}`, data);
+//   }
 
-  uploadImages(files: File[]): Observable<any[]> {
-    const observables: Observable<any>[] = [];
-    files.forEach(file => {
-      observables.push(this.completeDataToUpload(file));
-    });
-    return forkJoin(observables);
-  }
-}
+//   uploadImages(files: File[]): Observable<any[]> {
+//     const observables: Observable<any>[] = [];
+//     files.forEach(file => {
+//       observables.push(this.completeDataToUpload(file));
+//     });
+//     return forkJoin(observables);
+//   }
+// }
