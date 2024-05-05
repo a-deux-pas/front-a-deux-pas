776f65f807a50f633e8534bbf1a9a890c5a9b58f
LEA HADIDA <hadida.lea@gmail.com>
2024-05-05 21:58:05 +0200

    finalise default home header + created a categories component and a search bar component + code refactoring

--- src/app/routes/home/default-home/default-home.component.ts
+++ src/app/routes/home/default-home/default-home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
+import { CategoriesComponent } from './components/categories/categories.component';
 
 @Component({
   selector: 'app-default-home',
   templateUrl: './default-home.component.html',
   styleUrl: './default-home.component.scss',
   standalone: true,
-  imports: [HeaderComponent]
+  imports: [HeaderComponent, CategoriesComponent]
 })
 export class DefaultHomeComponent {
 
--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -2,13 +2,14 @@ import { Component, Output, EventEmitter, Input } from '@angular/core';
 import { accountRoutes } from '../../../routes/account/account-routing.module';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
   standalone: true,
-  imports: [RouterModule, CommonModule]
+  imports: [RouterModule, CommonModule, SearchBarComponent]
 })
 export class NavbarComponent {
   @Output() accountMenuToggleOutput: EventEmitter<void> =
