b10aca143e3b021aa05a6e28576ce1ae8cbf8c53
Erika IKELEMPO <erika.ikelempo@algonis.net>
2024-05-10 15:51:04 +0200

    removed useless files

--- src/app/shared/utils/credientials.ts
+++
@@ -1,3 +0,0 @@
-export const UPLOAD_PRESET: string = 'adeupasProject';
-export const CLOUD_NAME: string = 'erikaadeuxpas';
-export const CALL_TO_CLOUDINARY_API: string = 'https://api.cloudinary.com/v1_1/erikaadeuxpas/upload/';
