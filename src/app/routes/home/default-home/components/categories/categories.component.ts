662bbba5f56066d4cb0f49155d55c09bd4fc4cbb
LEA HADIDA <hadida.lea@gmail.com>
2024-05-11 20:09:08 +0200

    changes following Erika's feedback

--- src/app/routes/home/default-home/components/categories/categories.component.ts
+++ src/app/routes/home/default-home/components/categories/categories.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-categories',
   standalone: true,
-  imports: [],
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.scss'
 })
