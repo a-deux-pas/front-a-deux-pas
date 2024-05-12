0b5a84ca0805d62aa30c5bf8cac26c067364be47
LEA HADIDA <hadida.lea@gmail.com>
2024-05-12 12:40:55 +0200

    changes following Erika's feedback 2

---
+++ src/app/routes/home/logged-in-home/logged-in-home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logged-in-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './logged-in-home.component.html',
+  styleUrl: './logged-in-home.component.scss'
+})
+export class LoggedInHomeComponent {}
