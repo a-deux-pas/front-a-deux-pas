db7c0058bf55a5fb8aa0b147f87c04e3d2bacb46
LEA HADIDA <hadida.lea@gmail.com>
2024-05-13 13:40:16 +0200

    error message changes in ad-form component + added marging in my-ad component + moved 'ScrollToTop' méthod into my-ad component

--- src/app/routes/account/ads/my-ad/my-ad.component.ts
+++ src/app/routes/account/ads/my-ad/my-ad.component.ts
@@ -3,7 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { AdService } from '../../../Ad.service';
 import { AdPostResponse } from '../../../../shared/models/ad/ad-post-response.model';
 import { NgbCarousel, NgbCarouselModule, NgbNavModule, NgbSlideEvent, NgbSlideEventSource } from '@ng-bootstrap/ng-bootstrap';
-import { CommonModule } from '@angular/common';
+import { CommonModule, ViewportScroller } from '@angular/common';
 import { AdCardComponent } from '../../../../shared/components/ads/ad-card/ad-card.component';
 import { Subscription } from 'rxjs';
 import { DisplayManagementService } from '../../../../shared/services/display-management.service';
@@ -32,6 +32,7 @@ export class MyAdComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private adService: AdService,
+    private viewportScroller: ViewportScroller,
     private displayManagementService: DisplayManagementService
   ) {
     this.windowSizeSubscription = this.displayManagementService.isBigScreen$.subscribe(isBigScreen => {
@@ -40,6 +41,7 @@ export class MyAdComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.scrollToTop();
     const adId: number | null = Number(this.route.snapshot.paramMap.get(('id')));
     this.route.queryParams.subscribe(params => {
       if (params['success'] === 'true') {
@@ -63,13 +65,19 @@ export class MyAdComponent implements OnInit {
         this.adService.findMyAds(this.myAd.publisherId!).subscribe({
           next: (myOtherAds: AdPostResponse[]) => {
             this.myOtherAds = myOtherAds.filter(ad => ad.id !== this.myAd!.id);
+            console.log(this.myOtherAds);
           }
         })
+        console.log("ext méthode" + this.myOtherAds);
       },
       error: error => {
         console.error(error);
       }
     });
+  }
+
+  scrollToTop(): void {
+    this.viewportScroller.scrollToPosition([0, 0])
   }
 
   // image carrousel for mobile device
