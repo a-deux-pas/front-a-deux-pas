ffd0cd86529948748790e29160e711efa65653f7
LEA HADIDA <hadida.lea@gmail.com>
2024-05-13 17:02:51 +0200

    last Erika's feedback

--- src/app/routes/home/logged-in-home/logged-in-home.component.ts
+++ src/app/routes/home/logged-in-home/logged-in-home.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-logged-in-home',
   standalone: true,
-  imports: [],
   templateUrl: './logged-in-home.component.html',
   styleUrl: './logged-in-home.component.scss'
 })
--- src/app/shared/components/navbar/search-bar/search-bar.component.ts
+++ src/app/shared/components/navbar/search-bar/search-bar.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-search-bar',
   standalone: true,
-  imports: [],
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.scss'
 })
