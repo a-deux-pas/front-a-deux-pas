f6a5b962b4f53b9173d7f3be97a1f5818ae33fcc
Erika IKELEMPO <erika.ikelempo@algonis.net>
2024-05-18 11:13:46 +0200

    added ad page in commit

---
+++ src/app/shared/components/ads/ad-page/ad-page.component.ts
@@ -0,0 +1,163 @@
+import { ActivatedRoute } from '@angular/router';
+import { AdService } from '../../../../routes/ad/ad.service';
+import { AdPostResponse } from '../../../../shared/models/ad/ad-post-response.model';
+import { CommonModule, ViewportScroller } from '@angular/common';
+import { AdCardComponent } from '../../../../shared/components/ads/ad-card/ad-card.component';
+import { Component, OnInit, Input, ViewChild } from '@angular/core'
+import { SplitComponent, AngularSplitModule } from 'angular-split'
+import { UiModule } from '../../../../shared/module/ui/ui.module';
+import { NgbCarousel, NgbCarouselModule, NgbNavModule, NgbSlideEvent, NgbSlideEventSource } from '@ng-bootstrap/ng-bootstrap';
+import { CtaMyAdComponent } from '../cta-my-ad/cta-my-ad.component';
+import { Subscription } from 'rxjs'
+
+
+
+@Component({
+  selector: 'app-ad-page',
+  standalone: true,
+  imports: [
+    CommonModule,
+    AdCardComponent,
+    AngularSplitModule,
+    CommonModule,
+    UiModule,
+    NgbNavModule,
+    NgbCarouselModule,
+    CtaMyAdComponent
+  ],
+  templateUrl: './ad-page.component.html',
+  styleUrl: './ad-page.component.scss'
+})
+export class AdPageComponent implements OnInit {
+  @Input() adSuccessfullySubmitted: boolean | undefined;
+  @Input() isBigScreen: boolean | undefined;
+  @Input() windowSizeSubscription!: Subscription;
+  @Input() onMyAd: boolean | undefined;
+
+  // TO DO ::  à voir si le nom de la variable change pas pour prendre en compte myAd ou sellerAd
+  myAd: AdPostResponse | undefined;
+
+  selectedPicNumber: number = 2;
+  articlePictures: (string | undefined)[] = [];
+  areaSizeA!: number
+  areaSizeB!: number
+  @ViewChild('splitAreaA') splitAreaA!: SplitComponent
+  @ViewChild('splitAreaB') splitAreaB!: SplitComponent
+  showSeeMorBtn!: boolean;
+
+  adCount!: number;
+
+  constructor(
+    private route: ActivatedRoute,
+    private adService: AdService,
+    private viewportScroller: ViewportScroller
+  ) { }
+
+  ngOnInit(): void {
+    this.scrollToTop();
+    const adId: number | null = Number(this.route.snapshot.paramMap.get(('id')));
+    // TO DO :: à changer lors de l'US sellerAd
+    this.route.queryParams.subscribe(params => {
+      if (params['success'] === 'true') {
+        this.adSuccessfullySubmitted = true;
+        setTimeout(() => {
+          this.adSuccessfullySubmitted = false;
+        }, 3000);
+      }
+    });
+    this.adService.findAdById(adId).subscribe({
+      next: (ad: AdPostResponse) => {
+        this.myAd = ad;
+        this.articlePictures = [
+          this.myAd.firstArticlePictureUrl,
+          this.myAd.secondArticlePictureUrl,
+          this.myAd.thirdArticlePictureUrl,
+          this.myAd.fourthArticlePictureUrl,
+          this.myAd.fifthArticlePictureUrl
+        ].filter(url => !!url);
+        this.selectedPicNumber = this.articlePictures.length;
+        [this.areaSizeA, this.areaSizeB] = this.setSplitAreasSizes(this.articlePictures.length);
+        this.fetchPaginatedAdsList()
+        // TO DO : à changer une fois la connexion implémentée
+        this.adService.getMyAdsCount(1).subscribe({
+          next: (adCount: number) => {
+            this.adCount = adCount
+            this.showSeeMorBtn = this.adCount > 9
+          }
+        })
+      },
+      error: error => {
+        console.error(error);
+      }
+    });
+  }
+
+  scrollToTop(): void {
+    this.viewportScroller.scrollToPosition([0, 0])
+  }
+
+  setSplitAreasSizes(nPictures: number) {
+    switch (nPictures) {
+      case 3:
+        return [30, 70]
+      case 4:
+        return [25, 75]
+      case 5:
+        return [15, 85]
+      default:
+        return [50, 50]
+    }
+  }
+
+  @ViewChild('carousel', { static: true }) carousel!: NgbCarousel;
+
+  paused = false;
+  unpauseOnArrow = false;
+  pauseOnIndicator = false;
+  pauseOnHover = true;
+  pauseOnFocus = true;
+
+  togglePaused() {
+    if (this.paused) {
+      this.carousel!.cycle();
+    } else {
+      this.carousel!.pause();
+    }
+    this.paused = !this.paused;
+  }
+
+  onSlide(slideEvent: NgbSlideEvent) {
+    if (
+      this.unpauseOnArrow &&
+      slideEvent.paused &&
+      (slideEvent.source === NgbSlideEventSource.ARROW_LEFT || slideEvent.source === NgbSlideEventSource.ARROW_RIGHT)
+    ) {
+      this.togglePaused();
+    }
+    if (this.pauseOnIndicator && !slideEvent.paused && slideEvent.source === NgbSlideEventSource.INDICATOR) {
+      this.togglePaused();
+    }
+  }
+
+  pageNumber: number = 0;
+  pageSize!: number;
+  noMoreAds: boolean = false;
+  userOtherAds: AdPostResponse[] = [];
+
+  loadMoreAds() {
+    this.pageNumber++;
+    this.fetchPaginatedAdsList();
+  }
+
+  // TO DO :: à changer quand le processus de connexion sera implémenté
+  fetchPaginatedAdsList() {
+    this.pageSize = this.onMyAd ? 9 : 4;
+    this.adService.fetchMoreAds(1, this.pageNumber, this.pageSize).subscribe({
+      next: (ads: AdPostResponse[]) => {
+        this.userOtherAds = [...this.userOtherAds, ...ads];
+        this.userOtherAds = this.userOtherAds.filter(ad => ad.id !== this.myAd!.id);
+        this.noMoreAds = this.userOtherAds.length >= (this.adCount - 1)
+      }
+    });
+  }
+}
