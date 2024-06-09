c7450c389352b6896176257da052c6c8a36fb682
LEA HADIDA <hadida.lea@gmail.com>
2024-06-09 23:42:03 +0200

    customized navabar on registration page

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,6 +1,6 @@
 import { Component, Output, EventEmitter, Input } from '@angular/core';
 import { accountRoutes } from '../../../routes/account/account-routing.module';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { AuthService } from '../../services/auth.service';
@@ -18,8 +18,13 @@ export class NavbarComponent {
   @Input() isAccountMenuOpen: boolean = false;
   accountRoutes = accountRoutes;
   @Input() isLoggedIn: boolean = false;
-  
-  constructor(private authService: AuthService) {}
+  isRegistrationPage: boolean = false;
+
+  constructor(private authService: AuthService, private router: Router) {
+    this.router.events.subscribe(event => {
+      this.isRegistrationPage = this.router.url.includes('/inscription');
+    });
+  }
 
   emitToggleAccountMenu() {
     this.accountMenuToggleOutput.emit();
