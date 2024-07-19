452190500345397cf503393dd992350230bee678
LEA HADIDA <hadida.lea@gmail.com>
2024-07-19 21:47:50 +0200

    changed variable name and added unsubscribe

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -7,6 +7,7 @@ import { AuthService } from '../../services/auth.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ConnectionModalComponent } from '../connection-modal/connection-modal.component';
 import { AdService } from '../../services/ad.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
@@ -20,18 +21,23 @@ export class NavbarComponent implements OnInit {
     new EventEmitter<boolean>();
   @Input() isAccountMenuOpen: boolean = false;
   accountRoutes = accountRoutes;
-  @Input() isLoggedIn!: boolean
+  @Input() isUserLoggedIn!: boolean
   onSellerAdPageUnlogged: boolean = false;
   isRegistrationPage: boolean = false;
+  onSellerAdPageUnloggedSubscription!: Subscription;
 
-  constructor(private authService: AuthService, private router: Router, public modalService: NgbModal, private adService: AdService) {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    public modalService: NgbModal,
+    private adService: AdService) {
     this.router.events.subscribe(event => {
       this.isRegistrationPage = this.router.url.includes('/inscription');
     });
   }
 
   ngOnInit(): void {
-    this.adService.sellerAdPageLoaded$.subscribe((boolean) => {
+    this.onSellerAdPageUnloggedSubscription = this.adService.sellerAdPageLoaded$.subscribe((boolean) => {
       this.onSellerAdPageUnlogged = boolean;
     })
   }
@@ -50,10 +56,10 @@ export class NavbarComponent implements OnInit {
   }
 
   openModalOrSell() {
-    this.isLoggedIn ? this.router.navigate(['annonce/creation']) : this.openModal();
+    this.isUserLoggedIn ? this.router.navigate(['annonce/creation']) : this.openModal();
   }
 
   ngOnDestroy() {
-    this.adService.isOnSellerAdPageUnLogged(false);
+    this.onSellerAdPageUnloggedSubscription.unsubscribe();
   }
 }
