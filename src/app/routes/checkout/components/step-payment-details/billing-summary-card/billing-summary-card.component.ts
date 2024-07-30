89161d29b4bd96c74632595a19140ec9bffb5816
LEA HADIDA <hadida.lea@gmail.com>
2024-07-30 22:09:19 +0200

    added link to ad detail on order recap card and billing summary card

--- src/app/routes/checkout/components/step-payment-details/billing-summary-card/billing-summary-card.component.ts
+++ src/app/routes/checkout/components/step-payment-details/billing-summary-card/billing-summary-card.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CheckoutService } from '../../../checkout.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-billing-summary-card',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './billing-summary-card.component.html',
   styleUrl: './billing-summary-card.component.scss',
 })
