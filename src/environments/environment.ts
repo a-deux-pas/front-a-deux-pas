3d325327fb025603238a36aa942345ec1d0d2017
Mircea Bardan <mircea.c.bardan@gmail.com>
2024-09-05 00:44:55 +0200

    test tokens

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,8 @@
 export const environment = {
   production: true,
   API_URL: 'https://back-a-deux-pas-d9201fbf0f29.herokuapp.com/api',
-  mapboxToken: 'MAPBOX_TOKEN_PLACEHOLDER',
-  stripeToken: 'STRIPE_TOKEN_PLACEHOLDER',
+  mapboxToken:
+    'pk.eyJ1IjoibGVhLWgiLCJhIjoiY2x4amQ5NDduMW9ocjJwc2J6d2pneTNncSJ9.fW17THPkQA9eDUVJYNHq0w',
+  stripeToken:
+    'pk_test_51PehXqRqo0EYMuWs8RGL5PO2Rg5A6tIa1qPWgbDJsQtSvVDwXWEd7zRnrlcbKWs3RHdgDnkUYYxiAnk9GQosL17u006yqaLH8s',
 };
