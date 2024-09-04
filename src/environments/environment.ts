7416332ce929d99f176f066691404bf873af8117
Mircea Bardan <mircea.c.bardan@gmail.com>
2024-09-04 23:34:18 +0200

    fixed API_URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_URL: 'https://back-a-deux-pas-d9201fbf0f29.herokuapp.com/api/',
+  API_URL: 'https://back-a-deux-pas-d9201fbf0f29.herokuapp.com/api',
   mapboxToken: 'MAPBOX_TOKEN_PLACEHOLDER',
   stripeToken: 'STRIPE_TOKEN_PLACEHOLDER',
 };
