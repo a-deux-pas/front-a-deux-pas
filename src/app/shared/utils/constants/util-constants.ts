26bd078c201970a0519da90fd579983fc7f358ad
Mircea Bardan <mircea.c.bardan@gmail.com>
2024-09-04 20:36:06 +0200

    configured environment.ts files

--- src/app/shared/utils/constants/util-constants.ts
+++ src/app/shared/utils/constants/util-constants.ts
@@ -1,7 +1,8 @@
-export const API_URL: string = 'http://localhost:8081/api';
-export const AUTH_BASE_URL: string = `${API_URL}/auth`;
-export const USER_BASE_URL: string = `${API_URL}/users`;
-export const ACCOUNT_BASE_URL: string = `${API_URL}/account`;
-export const AD_BASE_URL = `${API_URL}/ads`;
-export const MEETING_BASE_URL = `${API_URL}/meetings`;
-export const PAYMENT_BASE_URL = `${API_URL}/payment`;
+import { environment } from '../../../../environments/environment';
+
+export const AUTH_BASE_URL: string = `${environment.API_URL}/auth`;
+export const USER_BASE_URL: string = `${environment.API_URL}/users`;
+export const ACCOUNT_BASE_URL: string = `${environment.API_URL}/account`;
+export const AD_BASE_URL = `${environment.API_URL}/ads`;
+export const MEETING_BASE_URL = `${environment.API_URL}/meetings`;
+export const PAYMENT_BASE_URL = `${environment.API_URL}/payment`;
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
-  production: false,
-  mapboxToken: '${{ secrets.MAPBOX_ACCESS_TOKEN }}',
-  stripeToken: '${{ secrets.STRIPE_TOKEN }}',
+  production: true,
+  API_URL: 'https://back-a-deux-pas-d9201fbf0f29.herokuapp.com/api/',
+  mapboxToken: 'MAPBOX_TOKEN_PLACEHOLDER',
+  stripeToken: 'STRIPE_TOKEN_PLACEHOLDER',
 };
