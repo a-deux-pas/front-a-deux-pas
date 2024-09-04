db6052da6b6988510a50613615339b92d424b2ca
Mircea Bardan <mircea.c.bardan@gmail.com>
2024-09-04 23:11:36 +0200

    fixed dist path

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static(__dirname + "/dist/front"));
+app.use(express.static(__dirname + "/dist/front/browser"));
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/front/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/front/browser/index.html"));
 });
 
 app.listen(process.env.PORT || 8080);
